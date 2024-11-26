394be2e7dc474385c2604ba55d2edf648f8a98b9
W1zar707 <er626677@gmail.com>
2024-11-26 10:58:20 +0400

    Обновленные приложение, были добавлены новые баги для исправления

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -15,6 +15,8 @@ namespace ConsoleApp8
         // {} - количество раз
         static string number = "Легче позвонить чем у кого-то занимать: +7 (917) 084-39-93";
         static string pattern = @"\+\d{1} \(\d{3}\) \d{3}-\d{2}-\d{2}";
+        static string email = "Моя почта: er626677@gmail.com";
+        static string emailPattern = @"[a-zA-Z0-9]+@[A-Za-z]+\.[A-Za-z]+$";
         public static void match_string (string input, string match)
         {
             Match m = Regex.Match(input, match);
@@ -27,9 +29,22 @@ namespace ConsoleApp8
                 Console.WriteLine("Номер не найден");
             }
         }
+        public static void email_pattern(string input, string match)
+        {
+            Match m = Regex.Match(input, match);
+            if (m.Success)
+            {
+                Console.WriteLine("Почта найдена!");
+            }
+            else
+            {
+                Console.WriteLine("Почта не найдена!");
+            }
+        }
         static void Main(string[] args)
         {
             match_string(number, pattern);
+            email_pattern(email, emailPattern);
         }
     }
 }
