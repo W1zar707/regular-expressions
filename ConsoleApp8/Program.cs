aa44f95aac8cc5d874c90fad99ba43be134beb13
W1zar707 <er626677@gmail.com>
2024-11-26 10:04:02 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        // \d - любой цифровой символ
+        // \w - это любая буква или цифра
+        // \. - что угодно
+        // {} - количество раз
+        static string number = "Легче позвонить чем у кого-то занимать: 8-800-555-35-35";
+        static string pattern = @"\d{1}-\d{3}-\d{3}-\d{2}-\d{2}";
+        public static void match_string (string input, string match)
+        {
+            Match m = Regex.Match(input, match);
+            if (m.Success) 
+            {
+                Console.WriteLine("Номер найден!");
+            }
+            else
+            {
+                Console.WriteLine("Номер не найден");
+            }
+        }
+        static void Main(string[] args)
+        {
+            match_string(number, pattern);
+        }
+    }
+}
